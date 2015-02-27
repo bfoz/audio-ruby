5ab77ad35f158682cebe7999d873ed13ea3f3330
Brandon Fosdick <bfoz@bfoz.net>
2015-02-27 14:49:16 -0800

    Added a samples accessor to AudioBufferList
    This makes it easier to retrieve the samples as either Floats or Integers, depending on the stream format.

--- lib/core_audio.rb
+++ lib/core_audio.rb
@@ -16,11 +16,39 @@ module CoreAudio
 	def bytes
 	    self[:mData].get_bytes(0, self[:mDataByteSize])
 	end
+
+	# @return [Array<Integer>]  an array of samples, converted to signed integers
+	def samples_int(bytes_per_channel)
+	    self[:mData].get_array_of_int16(0, self[:mDataByteSize]/bytes_per_channel)
+	end
+
+	# @return [Array<Float>]    an array of samples, converted to {Float}
+	def samples_float(bytes_per_channel)
+	    self[:mData].get_array_of_float32(0, self[:mDataByteSize]/bytes_per_channel)
+	end
     end
 
     class AudioBufferList < FFI::Struct
 	layout :mNumberBuffers, :uint32,
 	       :mBuffers, AudioBuffer
+
+	# @return [Array]   the buffers
+	def buffers
+	    raise("Can't handle multiple buffers yet") if self[:mNumberBuffers] > 1
+	    [self[:mBuffers]]
+	end
+
+	# Retrieve the samples as an {Array}, after converting to the requested type
+	# @param type		    [Symbol]    :float or :int. Convert the samples to the given type.
+	# @param bytes_per_channel  [Number]	the number of bytes for each sample
+	# @return [Array<Float,Integer>]	an array of samples, converted to {Float} or {Integer}
+	def samples(type, bytes_per_channel)
+	    if type == :float
+		buffers.map {|buffer| buffer.samples_float(bytes_per_channel)}.flatten
+	    else
+		buffers.map {|buffer| buffer.samples_int(bytes_per_channel)}.flatten
+	    end
+	end
     end
     # @endgroup
 end
